0fc9ecb9a803c0cb49596e7b767b69934815cc91
George Asmaro <g.asmaro@surreyschools.ca>
2022-09-23 14:00:11 -0700

    First push

---
+++ FirstProject/src/main/java/com/mycompany/firstproject/FirstProject.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.FirstProject;
+
+//Importing the scanner from external libraries
+import java.util.Scanner;
+/**
+ *
+ * @author g.asmaro
+ */
+public class FirstProject {
+    
+    
+public static void main (String[] args) {
+    //Adding a scanner called input into the project
+    Scanner input = new Scanner(System.in);
+    System.out.println("Start Menu");
+    System.out.println("1. Data Converter");
+    System.out.println("2. Grade Calculator");
+    System.out.println("3. For Loop");
+    System.out.println("4. While Loop");
+    System.out.println("5. Tax calculator");
+    System.out.println("6. Arrays/Lists");
+    System.out.println("7. ");
+    System.out.println("8. ");
+    System.out.println("9. ");
+}
+
+}
