15704a049313175b7db5969484919dae9ddf4f93
George Asmaro <g.asmaro@surreyschools.ca>
2022-09-23 14:53:40 -0700

    update

--- FirstProject/src/main/java/com/mycompany/firstproject/FirstProject.java
+++ FirstProject/src/main/java/com/mycompany/firstproject/FirstProject.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.FirstProject;
+
+package com.mycompany.firstproject;
 
 //Importing the scanner from external libraries
 import java.util.Scanner;
@@ -23,9 +20,11 @@ public static void main (String[] args) {
     System.out.println("4. While Loop");
     System.out.println("5. Tax calculator");
     System.out.println("6. Arrays/Lists");
-    System.out.println("7. ");
+    System.out.println("7. Exit");
     System.out.println("8. ");
     System.out.println("9. ");
+    input.nextInt();
+    
 }
 
 }
